5894df759742bd4e3f7515bad3a8ce6fbb01fc16
Kory Gutierrez-le <Kory.Le@kp.org>
2018-06-21 14:55:06 -0700

    added requirements and sample code

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print ('hello world')
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
+import os
+
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+chrome_driver_location = dirname + '/lib/chromedriver'
+
+# Create a Chrome Browser in Incognito
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+
+print chrome_driver_location
+
+browser = webdriver.Chrome(executable_path=chrome_driver_location, chrome_options=option)
+
+
+browser.get("https://github.com/livewell")
+
+assert "Github" in browser.title
+
+
+browser.close()
